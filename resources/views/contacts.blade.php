17f70ea51f4987ad65815213828d5ffb394fd0f1
Roman <sprite6121716@gmail.com>
2017-05-30 23:57:34 +0300

    Prj1
    Prj1

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    
+    <body class="page">
+                   <!— background images —> 
+                
+    
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>My contacts:</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #FFEBCD;
+                 color: #FFE4E1;
+                   background-image: url(/img1/fon2.jpg);
+                 font-family: 'Courgette', cursive;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+              
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #FFE4E1;
+                padding: 0 25px;
+                font-size: 19px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+            <div class="content">
+                <div class="title m-b-md">
+                    My contacts:
+                </div>
+
+                <div class="links">
+                    <a href="http://minilaravel.loc/contacts">+375296121716 (VELCOME)</a>
+                    <a href="http://minilaravel.loc/contacts">sprite6121716@gmail.com</a>
+                     <a href="http://minilaravel.loc/">Return to Home Page</a>
+                </div>
+            </div>
+        </div>
+        <footer class="row footer js-footer">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+                        <p class="copyright-text"> &copy; 2017 Waseem Inc.</p>  
+                    </div>                
+                </footer>
+                <section data-page-id="page-1" class="content js-content">
+                
+    </body>
+</html>
---
+++ resources/views/page.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>My business card site!</title>
+
+        <!-- Fonts -->
+ <!--    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css"> -->
+<link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #00FFFF;
+                 color: #FFE4E1;
+                   background-image: url(/img1/fon_n.jpg);
+           
+            font-family: 'Courgette', cursive;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #FF4500;
+                padding: 0 50px;
+                font-size: 25px;
+                font-weight: 600;
+                /*letter-spacing: .1rem;
+                */
+                letter-spacing: normal;
+                text-decoration: inherit;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 500px;
+            }
+        </style>
+    </head>
+    <body>
+    
+        <div class="flex-center position-ref full-height">
+            
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Waseem Roman's business card site.
+                  
+                </div>
+              
+
+                <div class="links">
+                    <a href="http://minilaravel.loc/contacts">Contacts</a>
+                    <a href="http://www.bbc.com/russian/news" target="_blank">News</a>
+                    <a href="http://minilaravel.loc/social">Social networks</a>
+                    <a href="https://github.com/Roman964" target="_blank">My GitHub</a>
+                    <a href="https://www.bsuir.by/ru/kaf-piks" target="_blank">Our University</a>
+
+                </div>
+            </div>
+        </div>
+         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+                        <p class="copyright-text"> &copy; 2017 Waseem Inc.</p>  
+                        
+                    </div>                
+    </body>
+</html>
